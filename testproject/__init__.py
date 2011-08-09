2386ca2b8f4971fe85ee6d61dd860e4f33ccfe6b
twidi <s.angel@twidi.com>
2011-08-10 01:13:36 +0800

    add a (really) minimalist test project

---
+++ testproject/__init__.py
@@ -0,0 +1,7 @@
+"""
+Very very simple test project for django-flag.
+
+The only thing it does is flag users. You must be logged (via the admin interface) to do that (no django-registration here)
+
+Will be improved...
+"""
---
+++ testproject/views.py
@@ -0,0 +1,13 @@
+from django.views.generic import ListView
+from django.contrib.auth.models import User
+
+class UserListView(ListView):
+    model = User
+    context_object_name = 'user_list'
+
+    def get_context_data(self, **kwargs):
+        context = super(UserListView, self).get_context_data(**kwargs)
+        context['request'] = self.request
+        return context
+
+
