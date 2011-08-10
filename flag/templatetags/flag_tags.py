2b59c29286b3533de655a8e4622d8fd97e014c28
twidi <s.angel@twidi.com>
2011-08-11 00:33:24 +0800

    add a helper to create form with initial data

--- flag/templatetags/flag_tags.py
+++ flag/templatetags/flag_tags.py
@@ -1,7 +1,6 @@
 from django import template
 
-from django.contrib.contenttypes.models import ContentType
-from flag.forms import FlagForm, FlagFormWithCreator
+from flag.forms import get_default_form
 from flag.views import get_next
 from flag.models import FlaggedContent
 
@@ -10,31 +9,8 @@ register = template.Library()
 
 @register.inclusion_tag("flag/flag_form.html", takes_context=True)
 def flag(context, content_object, creator_field=None):
-
     request = context['request']
-
-    # get the content type for the given object
-    content_type = ContentType.objects.get(
-        app_label = content_object._meta.app_label,
-        model = content_object._meta.module_name
-    )
-
-    # initial data for the form
-    initial=dict(
-        content_type = content_type.id,
-        object_pk = content_object.pk,
-    )
-
-    # by default, a class without creator_field
-    form_class = FlagForm
-
-    # we have a creator field ? add it to a better form class
-    if creator_field:
-        form_class = FlagFormWithCreator
-        initial['creator_field'] = creator_field
-
-    form = form_class(initial=initial)
-
+    form = get_default_form(content_object, creator_field)
     return dict(
             form = form,
             next = get_next(request)
