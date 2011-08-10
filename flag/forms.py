73aba56c8ea8130afb0b5b1cbc5e3e20b8d60f02
twidi <s.angel@twidi.com>
2011-08-10 18:33:24 +0200

    add a helper to create form with initial data

--- flag/forms.py
+++ flag/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.contenttypes.models import ContentType
 from flag.settings import ALLOW_COMMENTS
 
 class FlagForm(forms.Form):
@@ -17,4 +18,30 @@ class FlagFormWithCreator(FlagForm):
     object to be flagged.
     """
     creator_field = forms.CharField(widget=forms.HiddenInput)
+
+def get_default_form(content_object, creator_field=None):
+    """
+    Helper to get a form from the right class, with initial parameters set
+    """
+    # get the content type for the given object
+    content_type = ContentType.objects.get(
+        app_label = content_object._meta.app_label,
+        model = content_object._meta.module_name
+    )
+
+    # initial data for the form
+    initial=dict(
+        content_type = content_type.id,
+        object_pk = content_object.pk,
+    )
+
+    # by default, a class without creator_field
+    form_class = FlagForm
+
+    # we have a creator field ? add it to a better form class
+    if creator_field:
+        form_class = FlagFormWithCreator
+        initial['creator_field'] = creator_field
+
+    return form_class(initial=initial)
 
