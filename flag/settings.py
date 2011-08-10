ae35d6fd956fa8c25e6f8a7e084bb74f5df1cb56
twidi <s.angel@twidi.com>
2011-08-10 17:59:50 +0800

    FLAG_ALLOW_COMMENTS is True by default, for compatibility

--- flag/settings.py
+++ flag/settings.py
@@ -1,6 +1,6 @@
 from django import conf
 
-# Set FLAG_ALLOW_COMMENTS to True in settings to allow users to
-# comment their flags
-ALLOW_COMMENTS = getattr(conf.settings, 'FLAG_ALLOW_COMMENTS', False)
+# Set FLAG_ALLOW_COMMENTS to False in settings to allow users to
+# comment their flags (True by default)
+ALLOW_COMMENTS = getattr(conf.settings, 'FLAG_ALLOW_COMMENTS', True)
 
