914ce4dc76d2a9ba008494654f1e6403e3fd4696
twidi <s.angel@twidi.com>
2011-08-11 01:37:59 +0800

    Add a "confirm" view, to be used in "get" mode (the classic "flag" view is for the "post" mode), with will render a template with rhe form for confirmation. This template can be simply "flag/confirm.html", or, if you want one for a specific model, "flag/confirm_applabel_modelname.html"

--- flag/templatetags/flag_tags.py
+++ flag/templatetags/flag_tags.py
@@ -1,7 +1,7 @@
 from django import template
 
 from flag.forms import get_default_form
-from flag.views import get_next
+from flag.views import get_next, get_confirm_url_for_object
 from flag.models import FlaggedContent
 
 register = template.Library()
@@ -44,6 +44,7 @@ def can_be_flagged_by(content_object, user):
     This filter will return True if the given user can flag the given object.
     We check that the user is authenticated, but also that the
     LIMIT_SAME_OBJECT_FOR_USER is not raised
+    Usage: {% if some_object|can_by_flagged_by:request.user %}...{% endif %}
     """
     if not (user and user.is_active and user.is_authenticated()):
         return False
@@ -52,3 +53,15 @@ def can_be_flagged_by(content_object, user):
         return flagged_content.can_be_flagged_by_user(user)
     except:
         return False
+
+@register.filter
+def flag_confirm_url(content_object, creator_field=None):
+    """
+    This filter will return the url of the flag confirm page for the given object
+    Usage: {{ some_object|flag_confirm_url }}
+    Or, with a creator_field : {{ some_object|flag_confirm_url:"some_field" }}
+    """
+    try:
+        return get_confirm_url_for_object(content_object, creator_field)
+    except:
+        return ""
--- flag/urls.py
+++ flag/urls.py
@@ -1,6 +1,17 @@
+"""
+Urls for the django-flag app
+Add a line like above to your own urls.py:
+    ur(r'^flag/', include('flag.urls')),
+To use the "flag_confirm" we, you can use the "flag_confirm_url" filter
+in your template, or "get_confirm_url_for_object" helper in flag.views
+"""
+
 from django.conf.urls.defaults import *
 
 
 urlpatterns = patterns("",
+    url(r'(?P<app_label>\w+)/(?P<object_name>\w+)/(?P<object_id>\d+)/((?P<creator_field>\w+)/)?$',
+        "flag.views.confirm",
+        name="flag_confirm"),
     url(r"^$", "flag.views.flag", name="flag"),
 )
