9f3aae164ff9497dadc5a4313127a76234f1df94
Mathieu Pillard <m.pillard@liberation.fr>
2011-08-24 15:10:23 +0200

    Fix merge conflicts

--- flag/settings.py
+++ flag/settings.py
@@ -1,6 +1,6 @@
 from django import conf
 
-# Set FLAG_ALLOW_COMMENTS to False in settings to allow users to
+# Set FLAG_ALLOW_COMMENTS to False in settings to not allow users to
 # comment their flags (True by default)
 ALLOW_COMMENTS = getattr(conf.settings, 'FLAG_ALLOW_COMMENTS', True)
 
@@ -11,4 +11,5 @@ LIMIT_SAME_OBJECT_FOR_USER = getattr(conf.settings, 'FLAG_LIMIT_SAME_OBJECT_FOR_
 # Set FLAG_LIMIT_FOR_OBJECT to a number in settings to limit the times an
 # object can be flagged
 LIMIT_FOR_OBJECT = getattr(conf.settings, 'FLAG_LIMIT_FOR_OBJECT', 0)
+
 
